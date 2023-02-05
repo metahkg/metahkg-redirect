22f1edbc9d8483d686db882284b299eb1b7619ec
wcyat <wcyat@wcyat.me>
2023-02-05 23:45:28 +0800

    fix: catch redis errors

--- src/lib/rateLimit.ts
+++ src/lib/rateLimit.ts
@@ -4,12 +4,12 @@ import { redis } from "./redis";
 export async function rateLimit(ip: string): Promise<number> {
   const hashedIp = sha256(ip);
   const redisKey = `rate-limit-${hashedIp}`;
-  const rate = Number((await redis.get(redisKey)) || 0);
+  const rate = Number((await redis.get(redisKey).catch(console.error)) || 0);
   if (rate) {
-    await redis.incr(redisKey);
+    await redis.incr(redisKey).catch(console.error);
     return rate;
   } else {
-    await redis.set(redisKey, 1, "EX", 30);
+    await redis.set(redisKey, 1, "EX", 30).catch(console.error);
     return 1;
   }
 }
