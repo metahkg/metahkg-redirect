d306def630b2671d257acdd7bf48e8a7adf203ac
wcyat <wcyat@wcyat.me>
2023-02-05 15:07:20 +0800

    feat(frontend): tracking, cancel, reachability, improved loading and errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,20 +7,22 @@ import {
   Collapse,
   Container,
   Grid,
+  Loading,
 } from "@nextui-org/react";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import useSWR from "swr";
 import MetahkgLogo from "../components/logo";
 import { regex } from "../lib/regex";
 import { InfoData, UrlHausThreat } from "./api/info";
-import { faWarning } from "@fortawesome/free-solid-svg-icons";
+import { faCancel, faWarning } from "@fortawesome/free-solid-svg-icons";
 import { safebrowsing_v4 } from "@googleapis/safebrowsing";
 
 export default function Redirect() {
   const router = useRouter();
   const [timer, setTimer] = useState(5);
   const [disclaimer, setDisclaimer] = useState(false);
+  const [cancel, setCancel] = useState(false);
   const url = decodeURIComponent(String(router.query.url));
 
   const { data, error } = useSWR<InfoData>(
@@ -29,60 +31,93 @@ export default function Redirect() {
       url ? await fetch(url).then((res) => res.json()) : null
   );
 
+  const countdown = data && !("error" in data) && data.safe && data.reachable && !cancel;
+
   useEffect(() => {
-    if (data && !("error" in data) && data.safe && timer >= 1) {
+    if (countdown && timer >= 1) {
       const timerInterval = setInterval(() => {
         setTimer(timer - 1);
       }, 1000);
       return () => clearInterval(timerInterval);
     }
-  }, [data, timer]);
+  }, [countdown, timer]);
 
-  if (!data) {
-    return <Text>Loading...</Text>;
+  if (timer === 0 && countdown) {
+    window.location.assign(data?.redirectUrl || url);
   }
 
-  if (error || "error" in data)
-    return <Text>Error {("error" in data && data.error) || error}</Text>;
+  const body = useMemo(() => {
+    if (url === "undefined") {
+      return null;
+    }
 
-  if (!regex.url.test(url)) {
-    return <Text>Invalid url</Text>;
-  }
-
-  if (timer === 0) {
-    window.location.assign(data.redirectUrl || url);
-  }
-
-  const threat:
-    | safebrowsing_v4.Schema$GoogleSecuritySafebrowsingV4ThreatMatch
-    | UrlHausThreat = data.safebrowsingThreats[0] || data.urlhausThreats[0];
-
-  return (
-    <Container className="bg-[#222] min-h-[100vh] w-[100vw] max-w-[100vw] max-h-full overflow-y-scroll flex justify-center">
-      <Container className="flex flex-col items-center justify-center w-[70vw] mt-[50px] mb-[50px]">
-        <Text h1 className="flex items-center mb-[20px]">
-          <MetahkgLogo light height={60} width={60} />
-          Metahkg
+    if (!regex.url.test(url)) {
+      return (
+        <Text h4 color="error">
+          Invalid url
         </Text>
+      );
+    }
 
+    if (!data) {
+      return <Loading type="points-opacity" size="lg" />;
+    }
+
+    if (error || "error" in data) {
+      return (
+        <Text h4 color="error">
+          Error: {("error" in data && data.error) || error}
+        </Text>
+      );
+    }
+
+    const threat:
+      | safebrowsing_v4.Schema$GoogleSecuritySafebrowsingV4ThreatMatch
+      | UrlHausThreat = data.safebrowsingThreats[0] || data.urlhausThreats[0];
+
+    return (
+      <React.Fragment>
         <Text h4>You will be redirected to the following url:</Text>
         <code>{url}</code>
         <Grid.Container gap={2} className="flex flex-col items-center w-full">
           <Grid>
             <Collapse.Group
-              accordion={false}
               splitted
-              shadow
               className="min-w-[50vw]"
             >
+              {data.tracking && (
+                <Collapse title={<Text h4>Tracking parameters detected</Text>}>
+                  Metahkg Redirect detected tracking parameters in the URL.
+                  <br />
+                  Cleaned URL:
+                  <br />
+                  <code>{data.tidyUrl}</code>
+                </Collapse>
+              )}
               {data.redirects && (
                 <Collapse
                   title={<Text h4>Redirect URL detected</Text>}
                   subtitle={data.redirectUrl}
                 >
-                  Metahkg Redirect detected this url redirects to:
+                  Metahkg Redirect detected this URL redirects to:
                   <br />
                   <code>{data.redirectUrl}</code>
+                </Collapse>
+              )}
+              {!data.reachable && (
+                <Collapse
+                  title={
+                    <Text h4 color="warning">
+                      <FontAwesomeIcon icon={faWarning} /> URL not reachable
+                    </Text>
+                  }
+                >
+                  Metahkg Redirect cannot reach the URL.
+                  <br />
+                  Metahkg Redirect may have been blocked by the URL, or the URL
+                  does not exist.
+                  <br />
+                  Visit at your own risk.
                 </Collapse>
               )}
               {!data.safe && (
@@ -93,7 +128,7 @@ export default function Redirect() {
                       {data.safebrowsingThreats.length
                         ? "Google safebrowsing"
                         : "Urlhaus"}{" "}
-                      identified this url as a threat. Proceed with caution.
+                      identified this URL as a threat. Proceed with caution.
                     </Text>
                   }
                   subtitle="Click here for more information."
@@ -144,29 +179,83 @@ export default function Redirect() {
           </Container>
         )}
 
-        <Container className="flex justify-center">
-          {Boolean(data.redirectUrl) && (
-            <Link href={data.redirectUrl}>
+        <Grid.Container gap={1.5} justify="center">
+          {Boolean(data.tidyUrl) && (
+            <Grid>
+              <Link href={data.tidyUrl}>
+                <Button
+                  color="gradient"
+                  bordered
+                  disabled={!data.safe && !disclaimer}
+                >
+                  <Text className="!mx-[10px]">
+                    Proceed to cleaned URL
+                    {countdown && ` (in ${timer}s)`}
+                  </Text>
+                </Button>
+              </Link>
+            </Grid>
+          )}
+          {Boolean(data.redirectUrl) && !data.tracking && (
+            <Grid>
+              <Link href={data.redirectUrl}>
+                <Button
+                  color="gradient"
+                  bordered
+                  disabled={!data.safe && !disclaimer}
+                >
+                  <Text className="!mx-[10px]">
+                    Proceed directly
+                    {countdown &&
+                      !data.tracking &&
+                      ` (in ${timer}s)`}
+                  </Text>
+                </Button>
+              </Link>
+            </Grid>
+          )}
+          <Grid>
+            <Link href={url}>
               <Button
-                color="gradient"
-                bordered
+                color={"default"}
                 disabled={!data.safe && !disclaimer}
-                className="mr-[15px]"
               >
-                Proceed directly{data.safe && ` (in ${timer}s)`}
+                <Text className="!mx-[10px]">
+                  Proceed
+                  {countdown &&
+                    !data.redirects &&
+                    !data.tracking &&
+                    ` (in ${timer}s)`}
+                </Text>
               </Button>
             </Link>
+          </Grid>
+          {countdown && (
+            <Grid>
+              <Button
+                color="error"
+                onClick={() => {
+                  setCancel(true);
+                }}
+              >
+                <Text className="!mx-[10px]">
+                  <FontAwesomeIcon icon={faCancel} /> Cancel
+                </Text>
+              </Button>
+            </Grid>
           )}
-          <Link href={url}>
-            <Button
-              color={data.safe ? "default" : "error"}
-              disabled={!data.safe && !disclaimer}
-            >
-              Proceed{data.safe && !data.redirects && ` (in ${timer}s)`}
-            </Button>
-          </Link>
-        </Container>
-      </Container>
+        </Grid.Container>
+      </React.Fragment>
+    );
+  }, [countdown, data, disclaimer, error, timer, url]);
+
+  return (
+    <Container className="flex flex-col items-center justify-center w-[70vw] mt-[50px] mb-[50px]">
+      <Text h1 className="flex items-center mb-[20px]">
+        <MetahkgLogo light height={60} width={60} />
+        Metahkg Redirect
+      </Text>
+      {body}
     </Container>
   );
 }
