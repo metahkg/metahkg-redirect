9581c7cbe8711657954fd6c23c129367fe4efb9b
wcyat <wcyat@wcyat.me>
2023-02-08 08:05:07 +0800

    fix: remove private-ip
    (cherry picked from commit 069ab10e7661112fda6128939a4c8d601b2190bf)

--- src/lib/isForbiddenHost.ts
+++ src/lib/isForbiddenHost.ts
@@ -1,6 +1,5 @@
 import dns from "dns";
 import isLocalhost from "is-localhost-ip";
-import isPrivateIp from "private-ip";
 
 export async function isForbiddenHost(
   hostname: string
@@ -9,7 +8,7 @@ export async function isForbiddenHost(
     const ips: string[] = await new Promise((resolve, reject) =>
       dns.resolve(hostname, (err, ips) => (err ? reject(err) : resolve(ips)))
     );
-    if (ips.some((ip) => isLocalhost(ip) || isPrivateIp(ip))) {
+    if (ips.some((ip) => isLocalhost(ip))) {
       return true;
     }
     return false;
