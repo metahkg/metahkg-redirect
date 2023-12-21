5d9079bbb3203bb5dc188a44e71df0f6f04fbb1f
wcyat <wcyat@wcyat.me>
2023-12-21 13:35:01 +0800

    fix(threat): type

--- src/types/threat.ts
+++ src/types/threat.ts
@@ -3,6 +3,7 @@ export interface UrlHausThreat {
   dateadded: string;
   url: string;
   url_status: "online" | "offline";
+  last_online: string;
   threat: string;
   tags: string;
   urlhaus_link: string;
